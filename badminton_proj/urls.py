63e5bd39a3e6305c14f14481c6dc3b7ffdca74c1
Habib Ur Rehman <x24170658@student.ncirl.ie>
2025-10-13 00:00:28 +0200

    Urls update

--- badminton_proj/urls.py
+++ badminton_proj/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
  
     path("signup/", views.signup, name="signup"),
     path("login/", views.user_login, name="login"),
+    path("/", views.user_login, name="login"),
     path("logout/", views.user_logout, name="logout"),
 
-   
     path("courts/", views.court_list, name="court_list"),
     path("courts/add/", views.court_create, name="court_create"),
     path("courts/<int:pk>/edit/", views.court_edit, name="court_edit"),
