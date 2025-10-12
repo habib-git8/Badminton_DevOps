8b7e6171c4a40740af625ce6aaca70ce556186cc
Habib Ur Rehman <x24170658@student.ncirl.ie>
2025-10-13 00:42:13 +0200

    Update settings.py

--- badminton_proj/settings.py
+++ badminton_proj/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-475b%*df-58wwjq6@)e6$dhb1!us!-%_a1j2unu69)s(==q(c2'
+# SECRET_KEY = 'django-insecure-475b%*df-58wwjq6@)e6$dhb1!us!-%_a1j2unu69)s(==q(c2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
