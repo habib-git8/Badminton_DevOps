e9c050e109f2ab4745f67f1d78a3ec9de1f6c34a
Habib Ur Rehman <x24170658@student.ncirl.ie>
2025-10-13 00:08:41 +0200

    Update urls.py

--- badminton_proj/urls.py
+++ badminton_proj/urls.py
@@ -4,20 +4,14 @@ from booking import views
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-
- 
+    path("admin/", admin.site.urls), 
     path("signup/", views.signup, name="signup"),
     path("login/", views.user_login, name="login"),
-    path("/", views.user_login, name="login"),
     path("logout/", views.user_logout, name="logout"),
-
     path("courts/", views.court_list, name="court_list"),
     path("courts/add/", views.court_create, name="court_create"),
     path("courts/<int:pk>/edit/", views.court_edit, name="court_edit"),
     path("courts/<int:pk>/delete/", views.court_delete, name="court_delete"),
-
-    
     path("bookings/", views.booking_list, name="booking_list"),
     path("bookings/add/", views.booking_create, name="booking_create"),
     path("bookings/<int:pk>/edit/", views.booking_edit, name="booking_edit"),
